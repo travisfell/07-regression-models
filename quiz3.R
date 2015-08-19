a36508b92b0b6035474db8035c1d3881f66ce3c3
travisfell <tfell2001@gmail.com>
2015-08-18 22:11:02 -0500

    question 5 and 6

--- quiz3.R
+++ quiz3.R
@@ -18,3 +18,16 @@ summary(q1)
 # question 4 
 q4 <- lm(mpg ~ I(wt * 0.5) + factor(cyl), data = mtcars)
 summary(q4)$coef
+
+# question 5
+x <- c(0.586, 0.166, -.042, -.61, 4, 11.72)
+y <- c(.549, -.026, -.127, -.7, 51, 1.344)
+q5 <- lm(y ~ x)
+hatvalues(q5)
+plot(x, y)
+abline(q5)
+summary(q5)
+
+# question 6
+dfbetas(q5)
+
