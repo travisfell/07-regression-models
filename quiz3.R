8a93df3e51275f7a2ef3ee1da801bdd1526a34fa
travisfell <tfell2001@gmail.com>
2015-08-22 08:53:02 -0500

    course project changes
    ggpairs view of possible predictors to narrow down most relevant
    variables, multiple regression and plotting

--- quiz3.R
+++ quiz3.R
@@ -14,14 +14,22 @@ summary(q1)$coef
 
 # question 3
 summary(q1)
+mpgbycyl <- lm(mpg ~ cyl, mtcars)
+summary(mpgbycyl)
+mpgbycylwt <- lm(mpg ~ cyl + wt, mtcars)
+summary(mpgbycylwt)
+interaction(mpgbycyl, mpgbycylwt)
+#should be first response: small p-value, reject, no interaction term
 
 # question 4 
 q4 <- lm(mpg ~ I(wt * 0.5) + factor(cyl), data = mtcars)
 summary(q4)$coef
+plot(q4)
+# answer should be change in mpg/1 ton of wt by spec # of cylindars
 
 # question 5
-x <- c(0.586, 0.166, -.042, -.61, 4, 11.72)
-y <- c(.549, -.026, -.127, -.7, 51, 1.344)
+x <- c(0.586, 0.166, -0.042, -0.614, 11.72)
+y <- c(0.549, -0.026, -0.127, -0.751, 1.344)
 q5 <- lm(y ~ x)
 hatvalues(q5)
 plot(x, y)
@@ -30,4 +38,5 @@ summary(q5)
 
 # question 6
 dfbetas(q5)
+dfbeta(q5)
 
